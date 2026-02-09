42cd01f62ecd641fa3469031cc5d689c6e735378
Jithu Cherian Philip <jcp.jithu.xyz@gmail.com>
2026-02-09 10:24:34 +0000

    saveing

---
+++ components/navBar.tsx
@@ -0,0 +1,16 @@
+export default function NavBar(){
+    return(
+        <div className="bg-gray-600">
+            <div>
+                <p>Home</p>
+                {/*home */}
+            </div>
+            <div>
+                {/*Notifications */}
+            </div>
+            <div>
+                {/*Tasks */}
+            </div>
+        </div>
+    );
+}
